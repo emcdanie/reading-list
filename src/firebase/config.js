687e6ce93a24bc3716e0c3cdd6d4a7c44ccbd921
emcdanie <66241957+emcdanie@users.noreply.github.com>
2022-03-16 16:52:02 +0100

    install firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,23 @@
+import {initializeApp} from "firebase/app";
+import { getFirestore, collection, getDocs } from 'firebase/firestore'
+
+
+//init firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyDhN1NWtNttDr367J76jHycrqysr58Ixso",
+    authDomain: "readinglistapp-2bdd6.firebaseapp.com",
+    projectId: "readinglistapp-2bdd6",
+    storageBucket: "readinglistapp-2bdd6.appspot.com",
+    messagingSenderId: "373720711410",
+    appId: "1:373720711410:web:d2899b14502313d9f5d06b"
+};
+
+
+//init firebase
+
+const app = initializeApp(firebaseConfig);
+
+
+//init firestore
+const db = getFirestore(app);
+export {db}
